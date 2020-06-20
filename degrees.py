09c3037f046f247f8e479695e05f1a7b7eea4447
Patrick Vianna Rieger <patrickrieger019@gmail.com>
2020-06-20 13:41:50 -0300

    implementation of the shortest_path

--- degrees.py
+++ degrees.py
@@ -92,8 +92,47 @@ def shortest_path(source, target):
     If no possible path, returns None.
     """
 
-    # TODO
-    raise NotImplementedError
+    # Keep track of number of states explored
+    num_explored = 0
+
+    # Initialize frontier to just the starting position
+    start = Node(state=source, parent=None, action=None)
+    frontier = QueueFrontier()
+    frontier.add(start)
+
+    # Initialize an empty explored set
+    explored = set()
+
+    # Keep looping until solution found
+    while True:
+
+        # If nothing left in frontier, then no path
+        if frontier.empty():
+            print("Number of explored Nodes: {}.".format(num_explored))
+            return None
+
+        # Choose a node from the frontier
+        node = frontier.remove()
+        num_explored += 1
+
+        # If node is the target, then we have a solution
+        if node.state == target:
+            shortest_list = []
+            while node.parent is not None:
+                shortest_list.append((node.action, node.state))
+                node = node.parent
+            shortest_list.reverse()
+            print("Number of explored Nodes: {}.".format(num_explored))
+            return shortest_list
+
+        # Mark node as explored
+        explored.add(node.state)
+
+        # Add neighbors to frontier
+        for action, state in neighbors_for_person(node.state):
+            if not frontier.contains_state(state) and state not in explored:
+                child = Node(state=state, parent=node, action=action)
+                frontier.add(child)
 
 
 def person_id_for_name(name):
